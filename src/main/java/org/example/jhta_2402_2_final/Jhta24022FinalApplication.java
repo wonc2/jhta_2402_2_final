c05cab3df4a3af7c38fff5fe76cefeaef1dde85d
wonc <wonclee112@gmail.com>
2024-08-19 12:00:17 +0900

    Initial commit

---
+++ src/main/java/org/example/jhta_2402_2_final/Jhta24022FinalApplication.java
@@ -0,0 +1,13 @@
+package org.example.jhta_2402_2_final;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jhta24022FinalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Jhta24022FinalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/jhta_2402_2_final/Jhta24022FinalApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.jhta_2402_2_final;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jhta24022FinalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
