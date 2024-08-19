c97a80a503ce05c0928bc9d7fea7222394187155
gahyun1113 <tlsrkgus1113@gmail.com>
2024-08-19 12:41:08 +0900

    html, css, js 파일 추가

---
+++ src/main/java/org/example/jhta_2402_2_final/testController/TestController.java
@@ -0,0 +1,9 @@
+package org.example.jhta_2402_2_final.testController;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TestController {
+
+}
