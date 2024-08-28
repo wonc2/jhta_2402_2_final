b0d1336a4b1ef16cc3741e7156626507bfcf17e9
shinhyeok22 <shinhyeok22@gmail.com>
2024-08-28 17:46:13 +0900

    docs : 창고 관련 .sql 추가
    창고 테이블 만들고, 값 주입

--- src/main/java/org/example/jhta_2402_2_final/controller/distribution/DistributionController.java
+++ src/main/java/org/example/jhta_2402_2_final/controller/distribution/DistributionController.java
@@ -1,4 +1,17 @@
 package org.example.jhta_2402_2_final.controller.distribution;
 
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/distribution")
 public class DistributionController{
+
+
 }
--- src/main/java/org/example/jhta_2402_2_final/dao/distribution/DistributionDao.java
+++ src/main/java/org/example/jhta_2402_2_final/dao/distribution/DistributionDao.java
@@ -1,4 +1,7 @@
 package org.example.jhta_2402_2_final.dao.distribution;
 
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
 public interface DistributionDao {
 }
