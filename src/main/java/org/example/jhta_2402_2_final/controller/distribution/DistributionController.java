c83558dfa8532889e71a892a57c4565fcd350acd
shinhyeok22 <shinhyeok22@gmail.com>
2024-08-28 16:22:51 +0900

    docs : 유통관련 폴더 생성

---
+++ src/main/java/org/example/jhta_2402_2_final/controller/distribution/DistributionController.java
@@ -0,0 +1,4 @@
+package org.example.jhta_2402_2_final.controller.distribution;
+
+public class DistributionController{
+}
---
+++ src/main/java/org/example/jhta_2402_2_final/service/distribution/DistributionService.java
@@ -0,0 +1,4 @@
+package org.example.jhta_2402_2_final.service.distribution;
+
+public class DistributionService {
+}
