4ac650debd7dc89f1b5efd818d74ef675953f88b
wonc <wonclee112@gmail.com>
2024-08-28 17:43:55 +0900

    Feat: productMainPage.html 리스트 구현중

--- src/main/java/org/example/jhta_2402_2_final/controller/ProductController.java
+++ src/main/java/org/example/jhta_2402_2_final/controller/ProductController.java
@@ -1,15 +1,26 @@
 package org.example.jhta_2402_2_final.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.example.jhta_2402_2_final.service.ProductService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+import java.util.Map;
+
 @Controller
+@RequiredArgsConstructor
 @RequestMapping("/product")
 public class ProductController {
 
+    private final ProductService productService;
     @GetMapping("/main")
-    public String productMainPage(){
-        return "main/productMainPage";
+    public String productMainPage(Model model){
+
+        List<Map<String,Object>> productList = productService.findAll();
+        model.addAttribute("productList",productList);
+        return "product/productMainPage";
     }
 }
