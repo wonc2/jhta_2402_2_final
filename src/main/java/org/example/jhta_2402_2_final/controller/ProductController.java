72823602146ad9cd2979635d26cb5eb315e8a94f
HaeToon <gxg4205@gmail.com>
2024-08-28 17:19:07 +0900

    Feat : product demo page add

---
+++ src/main/java/org/example/jhta_2402_2_final/controller/ProductController.java
@@ -0,0 +1,15 @@
+package org.example.jhta_2402_2_final.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/product")
+public class ProductController {
+
+    @GetMapping("/main")
+    public String productMainPage(){
+        return "main/productMainPage";
+    }
+}
