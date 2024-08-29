4f821903d6a749d1118f8cab19e1144dfab28315
shinhyeok22 <shinhyeok22@gmail.com>
2024-08-29 10:08:11 +0900

    feat : 로그인, 로그아웃 템플릿 적용 + 로그인 성공 시 사용자 명 뜸

--- src/main/java/org/example/jhta_2402_2_final/controller/MemberController.java
+++ src/main/java/org/example/jhta_2402_2_final/controller/MemberController.java
@@ -7,6 +7,9 @@ import org.example.jhta_2402_2_final.model.dto.MemberDto;
 import org.example.jhta_2402_2_final.service.MemberService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,6 +22,25 @@ public class MemberController {
 
     private final MemberService memberService;
 
+
+    @ResponseBody
+    @GetMapping("/logTest")
+    public void logTest() {
+        // 인증된 유저 정보를 가져옴
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        // UserDetails 타입으로 캐스팅하여 유저의 상세 정보를 가져올 수 있음
+        if (authentication != null && authentication.getPrincipal() instanceof UserDetails) {
+            UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+            String username = userDetails.getUsername();
+            // 유저의 이름 출력
+            System.out.println("Authenticated user: " + username +"///");
+            System.out.println(">>>>>>>>>"+userDetails);
+        } else {
+            // 인증된 사용자가 없을 경우 처리
+            System.out.println("No authenticated user found.");
+        }
+    }
 
 
     @GetMapping("/login")
