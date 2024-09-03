ce44c7335fd718e081cf6c9caddc0d53ce65e4be
wonc <wonclee112@gmail.com>
2024-09-03 11:49:47 +0900

    Feat:

--- src/main/java/org/example/jhta_2402_2_final/controller/product/ProductCompanyController.java
+++ src/main/java/org/example/jhta_2402_2_final/controller/product/ProductCompanyController.java
@@ -1,29 +1,11 @@
 package org.example.jhta_2402_2_final.controller.product;
 
-import lombok.RequiredArgsConstructor;
-import org.example.jhta_2402_2_final.model.dto.CustomUserDetails;
-import org.example.jhta_2402_2_final.model.dto.common.SourceDto;
-import org.example.jhta_2402_2_final.service.product.ProductCompanyService;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-import java.util.Map;
-
 @Controller
-@RequiredArgsConstructor
 @RequestMapping("/product/company")
 public class ProductCompanyController {
-
-    private final ProductCompanyService productCompanyService;
-
     @GetMapping()
-    public String productMainPage() {
-        // 생산품 리스트 ( 일단 로그인 ID(UNIQUE) = 생산업체 Name으로 구분? )
-
-        return "product/productCompanyMainPage";
-    }
+    public String productMainPage() {return "product/productCompanyMainPage";}
 }
