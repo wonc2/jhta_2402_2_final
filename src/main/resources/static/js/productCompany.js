fa61f395e214e3deefc5b81196ded43a8eb5e1be
wonc <wonclee112@gmail.com>
2024-09-03 09:30:18 +0900

    Feat: 선택한 리스트별로 테이블 바꾸는 로직 추가

--- src/main/resources/static/js/productCompany.js
+++ src/main/resources/static/js/productCompany.js
@@ -1,91 +1,24 @@
-
-$(document).ready(function() {
+$(document).ready(function () {
     updateSourceSelectList();
+    getCompanySourceTable();
 
-    /* 테이블 가져옴 */
-    let companySourceTable = $('#companySourceTable').DataTable({
-        searching: false,
-        ajax: {
-            url: '/api/product/company/add',
-            dataSrc: 'companySourceList'
-        },
-        columns: [
-            { data: 'companySourceId'},
-            { data: 'companyName' },
-            { data: 'companyAddress' },
-            { data: 'sourceName' },
-            { data: 'sourcePrice' },
-            {
-                data: null,
-                render: function (data, type, row) {
-                    return '<button type="button" class="btn btn-primary btn-sm" data-action="produce">생산</button> | ' +
-                        '<button type="button" class="btn btn-info btn-sm" data-action="approve">가격 수정</button> | ' +
-                        '<button type="button" class="btn btn-danger btn-sm" data-action="cancel">등록 삭제</button>';
-                }
-            }
-        ],
-        columnDefs: [
-            { targets: 0, visible: false },  // 첫 번째 열 숨기기
-        ],
-        pagingType: 'simple_numbers',
-        language: {
-            paginate: {
-                next: '>', // 다음 페이지 버튼
-                previous: '<' // 이전 페이지 버튼
-            }
-        }
-    });
-
-    // let warehouseTable = $('#warehouseTable').DataTable({
-    //     searching: false,
-    //     ajax: {
-    //         url: '/api/product/company/produce',
-    //         dataSrc: ''
-    //     },
-    //     columns: [
-    //         { data: 'sourceWarehouseId' },
-    //         { data: 'sourceName' },
-    //         { data: 'sourceQuantity' },
-    //         { data: 'sourcePrice' },
-    //         { data: 'totalPrice' },
-    //         { data: 'produceDate'},
-    //         {
-    //             data: null,
-    //             render: function (data, type, row) {
-    //                 return '<button type="button" class="btn btn-primary btn-sm" data-action="manage">재고 관리</button>';
-    //             }
-    //         }
-    //     ],
-    //     pagingType: 'simple_numbers',
-    //     language: {
-    //         paginate: {
-    //             next: '>', // 다음 페이지 버튼
-    //             previous: '<' // 이전 페이지 버튼
-    //         }
-    //     }
-    // });
-
-    /* 생산품 등록 관련 스크립트 */
-    // 셀렉트 박스 변경 이벤트
-    $('#addSourceSelect').on('change', function() {
+    // 생산품 등록 모달 재료 입력 스크립트 ( 셀렉트나 인풋텍스트 중 하나만 할 수 있게 해줌 )
+    $('#addSourceSelect').on('change', function () {
         if ($(this).val() !== "directInput") {
-            $('#addSourceInput').val('').prop('disabled', true);  // 인풋 필드 비활성화 및 초기화
-            $('#addSourceSelect').prop('disabled', false);  // 셀렉트 박스 활성화
+            $('#addSourceInput').val('').prop('disabled', true);
+            $('#addSourceSelect').prop('disabled', false);
         } else {
-            $('#addSourceInput').prop('disabled', false).focus();  // 인풋 필드 활성화 및 포커스
+            $('#addSourceInput').prop('disabled', false).focus();
         }
     });
 
-    $('#addSourceBtn').on('click', function() {
-        let data = {
-            sourcePrice: $('#sourcePrice').val()
-        };
+    // 생산품 등록 스크립트( 생산품 등록하고 업데이트된 리스트 가져옴 )
+    $('#addSourceBtn').on('click', function () {
+        let data = { sourcePrice: $('#sourcePrice').val() };
 
         if ($('#addSourceInput').prop('disabled')) {
-            // 셀렉트 박스가 활성화된 경우 (옵션 선택)
             data.sourceId = $('#addSourceSelect').val();
         } else {
-            // 인풋 필드가 활성화된 경우 (직접 입력)
             data.sourceName = $('#addSourceInput').val();
         }
 
@@ -94,46 +27,40 @@ $(document).ready(function() {
             type: 'POST',
             contentType: 'application/json',
             data: JSON.stringify(data),
-            success: function(response) {
+            success: function () {
                 $('#addSourceModal').modal('hide');
-                companySourceTable.ajax.reload(null, false);  // 데이터 유지하면서 테이블 새로고침
+                $('#companySourceTable').DataTable().ajax.reload(null, false);
                 updateSourceSelectList();
             },
-            error: function(xhr, status, error) {
+            error: function (xhr, status, error) {
                 console.error('Error occurred:', error);
             }
         });
     });
-    /* - 생산품 등록 스크립트 끝 -*/
 
-    // Delete
-    $('#companySourceTable').on('click', 'button[data-action="cancel"]', function() {
-        // 선택된 행의 데이터를 가져옴
+    // 테이블 내 버튼 클릭 이벤트
+    $('#companySourceTable').on('click', 'button[data-action="cancel"]', function () {
         const row = $(this).closest('tr');
-        const data = companySourceTable.row(row).data();
-
+        const data = $('#companySourceTable').DataTable().row(row).data();
         const companySourceId = data.companySourceId;
 
-        // 삭제 확인 메시지
         if (confirm('정말로 이 항목을 삭제하시겠습니까?')) {
             $.ajax({
                 url: `/api/product/company/add/${companySourceId}`,
                 type: 'DELETE',
-                success: function(response) {
-                    companySourceTable.ajax.reload(null, false);  // 테이블 새로고침
+                success: function () {
+                    $('#companySourceTable').DataTable().ajax.reload(null, false);
                 },
-                error: function(xhr, status, error) {
+                error: function (xhr, status, error) {
                     console.error('Error occurred while deleting:', error);
                 }
             });
         }
     });
 
-    /* 생산 스크립트 */
-    $('#companySourceTable').on('click', 'button[data-action="produce"]', function() {
-        // Get the data for the row
+    $('#companySourceTable').on('click', 'button[data-action="produce"]', function () {
         const row = $(this).closest('tr');
-        const data = companySourceTable.row(row).data();
+        const data = $('#companySourceTable').DataTable().row(row).data();
 
         $('#produceSourceName').val(data.sourceName);
         $('#produceSourcePrice').val(data.sourcePrice);
@@ -141,45 +68,40 @@ $(document).ready(function() {
 
         $('#produceSourceModal').modal('show');
     });
-    $('#produceSourceBtn').on('click', function() {
+
+    $('#produceSourceBtn').on('click', function () {
         const sourceQuantity = $('#sourceQuantity').val();
         const sourcePriceId = $('#sourcePriceId').val();
 
-        // Create the request payload
         const data = {
             sourceQuantity: sourceQuantity,
             sourcePriceId: sourcePriceId
         };
 
-        // Make the AJAX request to the server
         $.ajax({
             url: '/api/product/company/produce',
             type: 'POST',
             contentType: 'application/json',
             data: JSON.stringify(data),
-            success: function(response) {
+            success: function () {
                 $('#produceSourceModal').modal('hide');
                 alert('생산 요청이 완료되었습니다.');
-                companySourceTable.ajax.reload(null, false);  // Reload the table
+                $('#companySourceTable').DataTable().ajax.reload(null, false);
             },
-            error: function(xhr, status, error) {
+            error: function (xhr, status, error) {
                 console.error('Error occurred:', error);
                 alert('생산 요청 중 오류가 발생했습니다.');
             }
         });
     });
-    /* 생산 스크립트 끝 */
-
-    /* 다른 리스트들 */
 
     // 드롭다운 메뉴 항목 선택 시 이벤트 처리
-    $('.dropdown-menu a').on('click', function(event) {
+    $('.dropdown-menu a').on('click', function (event) {
         event.preventDefault();
 
         const selectedValue = $(this).data('value');
         const selectedText = $(this).text();
 
-        // 드롭다운 버튼의 텍스트를 선택한 항목으로 변경
         $('#viewListDropDown').text(selectedText);
 
         if (selectedValue === 'companySourceList') {
@@ -188,79 +110,119 @@ $(document).ready(function() {
         } else if (selectedValue === 'warehouseList') {
             $('#companySourceTableContainer').hide();
             $('#warehouseTableContainer').show();
+            if (!$.fn.DataTable.isDataTable('#warehouseTable')) {
+                getWarehouseTable();  // warehouseTable 초기화
+            } else {
+                $('#warehouseTable').DataTable().ajax.reload(null, false);  // warehouseTable 재로드
+            }
         }
     });
-}); // -- document.ready 끝 --
+});
 
-// 생산품 등록 select list 업데이트 하는 메서드
+// Helper function
 function updateSourceSelectList() {
     $.ajax({
-        url: '/api/product/company/add',  // API URL
+        url: '/api/product/company/add',
         type: 'GET',
         contentType: 'application/json',
-        success: function(response) {
+        success: function (response) {
             const sources = response.sources;
             const selectBox = $('#addSourceSelect');
 
-            // 기존 옵션들 제거
             selectBox.empty();
+            selectBox.append($('<option>', { value: 'directInput', text: '직접입력' }));
 
-            // 기본 옵션 추가
-            selectBox.append($('<option>', {
-                value: 'directInput',
-                text: '직접입력'
-            }));
-
-            // 새로운 옵션들 추가
-            sources.forEach(function(source) {
-                const option = $('<option>', {
-                    value: source.sourceId,
-                    text: source.sourceName
-                });
+            sources.forEach(function (source) {
+                const option = $('<option>', { value: source.sourceId, text: source.sourceName });
                 selectBox.append(option);
             });
         },
-        error: function(xhr, status, error) {
+        error: function (xhr, status, error) {
             console.error('Error occurred while fetching sources:', error);
         }
     });
 }
 
-const ctx = document.getElementById('salesProductChart');
-new Chart(ctx, {
-    type: 'doughnut',
-    data: {
-        labels: ['감자', '삼겹살', '김치', '고구마', '고등어', '문어'],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            y: {
-                beginAtZero: true
+function getCompanySourceTable() {
+    $('#companySourceTable').DataTable({
+        ajax: {
+            url: '/api/product/company/add',
+            dataSrc: 'companySourceList'
+        },
+        columns: [
+            {data: 'companySourceId', render: data => data.substring(0, 8)},
+            {data: 'companyName'},
+            {data: 'companyAddress'},
+            {data: 'sourceName'},
+            {data: 'sourcePrice'},
+            {
+                data: null,
+                render: function () {
+                    return `
+                        <button type="button" class="btn btn-primary btn-sm" data-action="produce">생산</button> |
+                        <button type="button" class="btn btn-info btn-sm" data-action="approve">수정</button> |
+                        <button type="button" class="btn btn-danger btn-sm" data-action="cancel">삭제</button>
+                    `;
+                }
             }
-        }
-    }
-});
-const ctx2 = document.getElementById('salesProductPriceChart');
-new Chart(ctx2, {
-    type: 'bar',
-    data: {
-        labels: ['감자', '삼겹살', '김치', '고구마', '고등어', '문어'],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            y: {
-                beginAtZero: true
-            }
-        }
-    }
-});
+        ],
+        language: {emptyTable: '데이터가 없습니다.'}
+    });
+}
+
+function getWarehouseTable(){
+    $('#warehouseTable').DataTable({
+        ajax: {
+            url: '/api/product/company/produce',
+            dataSrc: ''
+        },
+        columns: [
+            {data: 'sourceWarehouseId', render: data => data.substring(0, 8) },
+            {data: 'sourceName'},
+            {data: 'sourceQuantity'},
+            {data: 'sourcePrice'},
+            {data: 'totalPrice'},
+            {data: 'produceDate'}
+        ],
+        language: {emptyTable: '데이터가 없습니다.'}
+    });
+}
+
+// const ctx = document.getElementById('salesProductChart');
+// new Chart(ctx, {
+//     type: 'doughnut',
+//     data: {
+//         labels: ['감자', '삼겹살', '김치', '고구마', '고등어', '문어'],
+//         datasets: [{
+//             label: '# of Votes',
+//             data: [12, 19, 3, 5, 2, 3],
+//             borderWidth: 1
+//         }]
+//     },
+//     options: {
+//         scales: {
+//             y: {
+//                 beginAtZero: true
+//             }
+//         }
+//     }
+// });
+// const ctx2 = document.getElementById('salesProductPriceChart');
+// new Chart(ctx2, {
+//     type: 'bar',
+//     data: {
+//         labels: ['감자', '삼겹살', '김치', '고구마', '고등어', '문어'],
+//         datasets: [{
+//             label: '# of Votes',
+//             data: [12, 19, 3, 5, 2, 3],
+//             borderWidth: 1
+//         }]
+//     },
+//     options: {
+//         scales: {
+//             y: {
+//                 beginAtZero: true
+//             }
+//         }
+//     }
+// });
