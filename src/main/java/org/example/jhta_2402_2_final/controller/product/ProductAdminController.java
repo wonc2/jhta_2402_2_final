462e02d0bad7f2f50471235252cb53e25d4b2f90
HaeToon <gxg4205@gmail.com>
2024-09-04 15:48:19 +0900

    Feat: product admin page pagination realized

--- src/main/java/org/example/jhta_2402_2_final/controller/product/ProductAdminController.java
+++ src/main/java/org/example/jhta_2402_2_final/controller/product/ProductAdminController.java
@@ -1,16 +1,12 @@
 package org.example.jhta_2402_2_final.controller.product;
 
 import lombok.RequiredArgsConstructor;
-import org.example.jhta_2402_2_final.model.dto.common.SourcePriceViewDto;
 import org.example.jhta_2402_2_final.model.dto.product.ProductCompanyInsertDto;
-import org.example.jhta_2402_2_final.model.dto.product.ProductOrderViewDto;
 import org.example.jhta_2402_2_final.service.product.ProductAdminService;
-import org.example.jhta_2402_2_final.util.Pagination;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequiredArgsConstructor
@@ -20,64 +16,7 @@ public class ProductAdminController {
     private final ProductAdminService productAdminService;
 
     @GetMapping("/main")
-    public String productMainPage(Model model , @RequestParam(value = "page" , defaultValue = "1") int page , @RequestParam(value = "pageScale" , defaultValue = "10") int pageScale,@RequestParam(value="productTableStatus" , defaultValue = "order") String productTableStatus) {
-        System.out.println("productTableStatus=="+productTableStatus);
-        if(productTableStatus.equals("order")){
-            List<ProductOrderViewDto>productOrderList = productAdminService.getProductOrderList();
-            List<ProductOrderViewDto>paginatedOrderList = List.of();
-            Pagination pagination = Pagination.builder()
-                    .totalContent(productOrderList.size())
-                    .currentPage(page)
-                    .pageScale(pageScale)
-                    .blockScale(5)
-                    .totalPage((int)Math.ceil((double)productOrderList.size()/pageScale))
-                    .build();
-            int currentBlock = (int) Math.ceil((double) pagination.getCurrentPage() / pagination.getBlockScale());
-            int startPage = (currentBlock - 1) * pagination.getBlockScale() + 1;
-            System.out.println("startpage=="+startPage);
-            int endPage = Math.min(startPage + pagination.getBlockScale() - 1, pagination.getTotalPage());
-            System.out.println("endpage ==" + endPage);
-            pagination.setStartPage(startPage);
-            pagination.setEndPage(endPage);
-
-            int start = (page-1)*pageScale;
-            int end = Math.min(page * pageScale, pagination.getTotalContent());
-            if (start < pagination.getTotalContent()) {
-                paginatedOrderList = productOrderList.subList(start, end);
-            } else {
-                paginatedOrderList = List.of(); // 잘못된 페이지의 경우 빈 리스트 반환
-            }
-            model.addAttribute("pagination",pagination);
-            model.addAttribute("paginatedOrderList",paginatedOrderList);
-        }
-        if(productTableStatus.equals("production")){
-            List<SourcePriceViewDto>sourcePriceList = productAdminService.getProductSourceList();
-            List<SourcePriceViewDto>paginatedSourcePriceList = List.of();
-            Pagination pagination = Pagination.builder()
-                    .totalContent(sourcePriceList.size())
-                    .currentPage(page)
-                    .pageScale(pageScale)
-                    .blockScale(5)
-                    .totalPage((int)Math.ceil((double)sourcePriceList.size()/pageScale))
-                    .build();
-            int currentBlock = (int) Math.ceil((double) pagination.getCurrentPage() / pagination.getBlockScale());
-            int startPage = (currentBlock - 1) * pagination.getBlockScale() + 1;
-            System.out.println("startpage=="+startPage);
-            int endPage = Math.min(startPage + pagination.getBlockScale() - 1, pagination.getTotalPage());
-            System.out.println("endpage ==" + endPage);
-            pagination.setStartPage(startPage);
-            pagination.setEndPage(endPage);
-
-            int start = (page-1)*pageScale;
-            int end = Math.min(page * pageScale, pagination.getTotalContent());
-            if (start < pagination.getTotalContent()) {
-                paginatedSourcePriceList = sourcePriceList.subList(start, end);
-            } else {
-                paginatedSourcePriceList = List.of(); // 잘못된 페이지의 경우 빈 리스트 반환
-            }
-            model.addAttribute("pagination",pagination);
-            model.addAttribute("paginatedSourcePriceList",paginatedSourcePriceList);
-        }
+    public String productMainPage() {
         return "product/productAdminMainPage";
     }
 
