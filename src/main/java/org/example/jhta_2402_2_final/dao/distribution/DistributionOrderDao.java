469f6474b9c2e4b56b26f85f2bc98e4a2ab22ab3
lee0502 <i63774071@gmail.com>
2024-09-11 17:49:25 +0900

    "FEAT : 유통관리 주문 수정 "

--- src/main/java/org/example/jhta_2402_2_final/dao/distribution/DistributionOrderDao.java
+++ src/main/java/org/example/jhta_2402_2_final/dao/distribution/DistributionOrderDao.java
@@ -2,6 +2,7 @@ package org.example.jhta_2402_2_final.dao.distribution;
 
 import org.apache.ibatis.annotations.Mapper;
 import org.example.jhta_2402_2_final.model.dto.distribution.*;
+import org.example.jhta_2402_2_final.model.dto.distribution.MinPriceOrderDto;
 
 import java.util.List;
 import java.util.UUID;
@@ -15,17 +16,30 @@ public interface DistributionOrderDao {
     List<KitOrderNameDto> selectKitNameOrder();
 
     //kit_oder 테이블 조인해서 밀키트 재료 셀렉
-    List<KitOrderSourceNameDto> selectSourceNameOrder ();
+    List<KitOrderSourceNameDto> selectSourceNameOrder();
 
     //kti_order sourcePrice 테이블로 가격 셀랙
-    List<MinPriceSourceDto> selectMinPriceSource ();
-
+    List<MinPriceSourceDto> selectMinPriceSource();
 
     UUID selectSourcePriceId(UUID sourceId, UUID productCompanyId);
 
-    int insertProductOrder(UUID productOrderId, UUID productCompanyId, UUID sourceId, int totalQuantity, int sourcePrice, UUID kitOrderId);
-    int insertProductOrderLog(UUID productOrderId);
+    List<MinPriceOrderDto> selectKitOrderId();
+
+
+    int insertProductOrder(UUID productOrderId, UUID productCompanyId, UUID sourceId,
+                           int quantity,int price,UUID kitOrderId);
+int insertProductOrder2(IngredientDto ingredientDto);
+    int insertProductOrderLog(String productOrderId);
+
+    String getProductOrderId(IngredientDto ingredientDto);
 
 
     void updateKitOrderStatus(UUID kitOrderId);
+
+    List<IngredientDto> getIngredientsByKitOrderId(UUID kitOrderId);
+
+
+    UUID selectProductCompanyIdByName(String supplierName);
+
+    UUID selectSourceIdByName(String ingredientName);
 }
